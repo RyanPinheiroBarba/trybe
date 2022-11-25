02a9808d414a32269c1f190ac8ce2e9cfb380fd1
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2022-11-25 15:13:51 -0300

    Criando e copiando o teste 2 para resolver.

---
+++ 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/testes.js/teste2.js
@@ -0,0 +1,11 @@
+/*
+No exemplo abaixo, temos as informações de um paciente. Utilize o operador typeof para imprimir qual o tipo das variáveis patientId, isEnrolled, patientInfo e patientEmail. Esse operador retorna qual o tipo de uma variável, objeto, função ou expressão? Exemplo: console.log(typeof patientId) retornará number.
+*/
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
