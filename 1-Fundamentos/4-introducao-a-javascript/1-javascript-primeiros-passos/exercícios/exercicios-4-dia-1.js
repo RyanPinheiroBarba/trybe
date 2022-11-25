4f824c9c9f4016c89295ccd8dbce9abea8c32656
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2022-11-25 19:55:37 -0300

    Exercício 4.1-1 consluído

--- 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/exercícios/exercicios-4-dia-1.js
+++ 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/exercícios/exercicios-4-dia-1.js
@@ -7,17 +7,3 @@ console.log(a*b);
 console.log(a/b);
 console.log(a%b);
 console.log(a**b);
-
-if (a > b){
-  console.log("A variável a é maior que b");
-}else{
-  console.log("A variável b é maior que a");
-}
-
-if (a > 0){
-  console.log("positive");
-} else if (a<0){
-  console.log("negative");
-} else {
-  console.log("0");
-}
