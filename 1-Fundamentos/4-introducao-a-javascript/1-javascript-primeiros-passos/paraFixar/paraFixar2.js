286932a554c5d5f260c2ee67bdbd9763110c8935
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2022-11-25 14:30:19 -0300

    Para fixar 2 completo.

---
+++ 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/paraFixar/paraFixar2.js
@@ -0,0 +1,22 @@
+/*
+Agora que você já conhece os operadores aritméticos básicos do JavaScript, vamos praticá-los? Você pode consultar a lista de operadores na tabela JavaScript Arithmetic Operators disponível nesse link se tiver dúvidas.
+
+Vamos fazer algumas operações simples para encontrarmos a área e o perímetro de um retângulo de base 5 e altura 8.
+
+Crie uma constante chamada base e atribua a ela o valor 5.
+Crie uma constante uma chamada heigth e atribua a ela o valor 8.
+Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela heigth. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+*/
+
+const base = 5;
+const heigth = 8;
+
+const area = (base * heigth) /2;
+const perimeter = (base + heigth + heigth);
+
+console.log(base, heigth);
+
+console.log(`O resultado da area do triangulo de base ${base} e altura ${heigth} é igual a: ${area}`);
+
+console.log(`O resultado do perímetro do triangulo de base ${base} e altura ${heigth} é igual a: ${perimeter}`);
---
+++ 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/testes.js/teste1.js
@@ -0,0 +1,13 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientAge);
+
+patientId = '50';
+
+console.log(typeof patientId);
