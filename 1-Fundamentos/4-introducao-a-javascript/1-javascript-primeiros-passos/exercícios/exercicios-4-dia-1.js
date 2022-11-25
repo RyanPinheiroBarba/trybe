8f753dfecdb48dd17aa77acd7473aede171b86d2
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2022-11-25 19:50:45 -0300

    Exercício 2 de comparação concluído

---
+++ 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/exercícios/exercicios-4-dia-1.js
@@ -0,0 +1,16 @@
+const a = 6;
+const b = 2;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+if (a > b){
+  console.log("A variável a é maior que b");
+}else{
+  console.log("A variável b é maior que a");
+}
+
