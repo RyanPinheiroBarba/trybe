64be9a2f260b89e6ef5fe00ee1e858a8940f1b4c
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2022-11-29 02:12:00 -0300

    WIP: Calculando a alíquota do IR baseado no salário base.

--- 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/exercícios/exercicios-bonus-4.1-4.js
+++ 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/exercícios/exercicios-bonus-4.1-4.js
@@ -1,5 +1,37 @@
-let salarioBruto = Math.floor(Math.random()*10000).toFixed(2);
-let aliquota = Number;
-let impostoRenda = Number;
+let grossSalary = 12000.00;
+let aliquotINSS;
+let aliquotIR;
+let messageINSS;
+let message;
 
-if
+
+if (grossSalary <= 1556.94){
+  aliquotINSS = grossSalary * 0.08;
+  messageINSS = `Valor da alíquota de INSS = ${aliquotINSS}!`;
+  console.log(message);
+} else if (grossSalary <= 2594.92){
+  aliquotINSS = grossSalary * 0.09;
+  messageINSS = `Valor da alíquota de INSS = ${aliquotINSS}!`;
+} else if (grossSalary <= 5189.82){
+  aliquotINSS = grossSalary * 0.11;
+  messageINSS = `Valor da alíquota de INSS = ${aliquotINSS}!`;
+} else {
+  aliquotINSS = 570.88;
+  messageINSS = `Valor da alíquota de INSS = ${aliquotINSS}!`;
+}
+
+console.log(messageINSS);
+
+let baseSalary = grossSalary - aliquotINSS;
+
+if(baseSalary <= 1903.98){
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65){
+  aliquotIR = (baseSalary * 0.075) - 142.80;
+} else if(baseSalary <= 3751.05){
+  aliquotIR = (baseSalary * 0.15) - 354.80;
+} else if(baseSalary <= 4664.68){
+  aliquotIR = (baseSalary * 0.225) - 636.13;
+} else {
+  aliquotIR = (baseSalary * 0.275) - 869.36;
+}
