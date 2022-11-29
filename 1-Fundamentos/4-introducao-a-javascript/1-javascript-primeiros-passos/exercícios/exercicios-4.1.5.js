655c7491dbcfa1f897433da53a0dde0532dc11f1
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2022-11-28 21:04:38 -0300

    exercícios 4.1-5 pronto

---
+++ 1-Fundamentos/4-introducao-a-javascript/1-javascript-primeiros-passos/exercícios/exercicios-4.1.5.js
@@ -0,0 +1,13 @@
+let chessPiece = "Cavalo";
+let chessPieceMove = "";
+
+switch (chessPiece.toLowerCase()){
+  case chessPiece = "peão":
+    chessPieceMove = "1 Casa para frente, captura na diagonal."
+    console.log(chessPieceMove);
+    break;
+  case chessPiece = "cavalo":
+    chessPieceMove = "3 casas fazendo um ângulo de 90º de descolamento."
+    console.log(chessPieceMove);
+    break
+}
