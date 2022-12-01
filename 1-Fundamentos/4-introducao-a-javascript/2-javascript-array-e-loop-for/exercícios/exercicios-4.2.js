e248a37189201505d878f39f5d9d5cbd27db5183
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2022-12-01 20:05:41 -0300

    WIP: resolvendo somas dos valores de arrays com 3 métodos de for

--- 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/exercícios/exercicios-4.2.js
+++ 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/exercícios/exercicios-4.2.js
@@ -1,3 +1,22 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 console.log(numbers);
+
+let sum = 0;
+let sum2 = 0;
+let sum3 = 0;
+
+for (value of numbers){
+  sum += value;
+}
+console.log(sum)
+
+for (value in numbers){
+  sum2 += numbers[value];
+}
+console.log(sum2);
+
+for (let index = 0; index < numbers.length; index += 1){
+  sum3 += numbers[index];
+}
+console.log(sum3);
