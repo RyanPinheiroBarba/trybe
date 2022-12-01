0a30ec652c80395639bbb09c6dc9ee2b84f5e346
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2022-12-01 20:19:16 -0300

    WIP: resolvendo a média aritmética com for of.

--- 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/exercícios/exercicios-4.2.js
+++ 1-Fundamentos/4-introducao-a-javascript/2-javascript-array-e-loop-for/exercícios/exercicios-4.2.js
@@ -2,21 +2,38 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 console.log(numbers);
 
+//-------------- usando for of
+
 let sum = 0;
-let sum2 = 0;
-let sum3 = 0;
 
 for (value of numbers){
   sum += value;
 }
 console.log(sum)
 
+//--------------- usando for in
+
+let sum2 = 0;
+
 for (value in numbers){
   sum2 += numbers[value];
 }
 console.log(sum2);
 
+//---------------- usando for
+
+let sum3 = 0;
+
 for (let index = 0; index < numbers.length; index += 1){
   sum3 += numbers[index];
 }
 console.log(sum3);
+
+//----------------- média aritmética for of
+
+let arithmeticSum = 0;
+
+for (value of numbers){
+  arithmeticSum += value / numbers.length;
+}
+console.log(arithmeticSum.toFixed(2));
