511fd07e461d41df704aa951850ebc1b5880a457
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2022-12-15 19:28:40 -0300

    feat: criando as regras do evento de click nos botões de acertos e erros.

--- 1-Fundamentos/4-introducao-a-javascript/ResumaoJs/scripts.js
+++ 1-Fundamentos/4-introducao-a-javascript/ResumaoJs/scripts.js
@@ -1,7 +1,17 @@
 let fullCourt = document.getElementById("full-court");
 let hitsMistakes = document.getElementById("hits-mistakes");
 let clearButton = document.getElementById("clearButton");
-
-console.log(fullCourt);
-console.log(hitsMistakes);
-console.log(clearButton);
+let selectedColor;
+let shot;
+hitsMistakes.addEventListener("click", (event) => {
+  // console.log(event.target.innerText);
+  if(event.target.innerText === "ACERTOS") {
+    selectedColor = "green";
+    shot = "Acertou";
+  }
+  if(event.target.innerText === "ERROS") {
+    selectedColor = "red";
+    shot = "Errou";
+  }
+  console.log(selectedColor, shot);
+});
