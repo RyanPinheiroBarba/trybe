5ba14e36ba64185aff70073d8740d0716df1cab2
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2022-12-15 19:04:45 -0300

    pegando os elementos quadra, botões de arremesso e botão de limar usando html collection

---
+++ 1-Fundamentos/4-introducao-a-javascript/ResumaoJs/scripts.js
@@ -0,0 +1,7 @@
+let fullCourt = document.getElementById("full-court");
+let hitsMistakes = document.getElementById("hits-mistakes");
+let clearButton = document.getElementById("clearButton");
+
+console.log(fullCourt);
+console.log(hitsMistakes);
+console.log(clearButton);
