44cf5f9d030e337b1a70a1713185ea93befbeddd
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2022-12-15 20:40:18 -0300

    feat: criando a função de adicionar uma div na posição do click

--- 1-Fundamentos/4-introducao-a-javascript/ResumaoJs/scripts.js
+++ 1-Fundamentos/4-introducao-a-javascript/ResumaoJs/scripts.js
@@ -1,6 +1,8 @@
 let fullCourt = document.getElementById("full-court");
 let hitsMistakes = document.getElementById("hits-mistakes");
 let clearButton = document.getElementById("clearButton");
+let allShots = document.getElementById("all-shots");
+
 let selectedColor;
 let shot;
 hitsMistakes.addEventListener("click", (event) => {
@@ -15,3 +17,15 @@ hitsMistakes.addEventListener("click", (event) => {
   }
   console.log(selectedColor, shot);
 });
+
+const addShotToCourt = (event) => {
+  let div = document.createElement("div");
+  div.style.position = "absolute";
+  div.style.top = `${event.pageY}px`;
+  div.style.left = `${event.pageX}px`;
+  div.style.width = "20px";
+  div.style.height = "20px";
+  div.style.border = "50% 0 solid red";
+  div.style.backgroundColor = selectedColor;
+  allShots.appendChild(div);
+}
