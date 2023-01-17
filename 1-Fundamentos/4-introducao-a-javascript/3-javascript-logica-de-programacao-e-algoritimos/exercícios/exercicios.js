c46c0636a2ea934487318d39df788a92b42e4060
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2023-01-17 09:45:32 -0300

    feat: criando uma função que divida os caracteres de uma string, dentro um array, conte os caracteres e diga qual é a maior palavra.

--- 1-Fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercícios/exercicios.js
+++ 1-Fundamentos/4-introducao-a-javascript/3-javascript-logica-de-programacao-e-algoritimos/exercícios/exercicios.js
@@ -4,3 +4,18 @@ const reverseArray = arrayWord.reverse();
 const joinArray = reverseArray.join("");
 
 console.log(joinArray);
+
+const arrayString = ["java", "javascript", "python", "html", "css"];
+let biggerString = [arrayString[0]];
+
+const splitAndPush = () => {
+  for (let index = 0; index < arrayString.length; index += 1) {
+    const splitPhrase = arrayString[index].split("");
+    if (splitPhrase.length > biggerString.length) {
+      biggerString = splitPhrase.join("");
+    }
+  }
+  console.log(biggerString);
+};
+
+splitAndPush();
