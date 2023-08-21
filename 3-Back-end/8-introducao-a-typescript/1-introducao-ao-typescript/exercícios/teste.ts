c6015a27ccbf6742878fc42ed679a27c27a765c2
RyanPinheiroBarba <ryanpinheiro1991@gmail.com>
2023-08-21 17:08:44 -0300

    Iniciando estudos de Typescript

---
+++ 3-Back-end/8-introducao-a-typescript/1-introducao-ao-typescript/exercícios/teste.ts
@@ -0,0 +1,5 @@
+let minhaIdade = 83; // Essa variável contém um número
+let meuNome = "Patinhas McPatinhas"; // Essa contém uma string
+
+console.log(meuNome.toLowerCase());
+// console.log(minhaIdade.toLowerCase()); // Erro! Não é possível chamar o método toLowerCase() em um número, apenas em strings;
---
+++ 3-Back-end/8-introducao-a-typescript/1-introducao-ao-typescript/exercícios/teste2.ts
@@ -0,0 +1,3 @@
+let firstName = 'Julia';
+// firstName = true; // Erro! Não é possível atribuir um booleano a uma variável do tipo string;
+console.log(typeof(firstName));
